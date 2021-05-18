3e981acc5e7b37abf2f5572b28f67cd762a23cc1
Matthew Horridge <matthew.horridge@stanford.edu>
2021-05-18 11:04:30 -0700

    Merged webprotege-server-common into webprotege-server-core

--- webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/shared/dispatch/ActionExecutionResult_TestCase.java
+++ webprotege-server-core/src/test/java/edu/stanford/bmir/protege/web/shared/dispatch/ActionExecutionResult_TestCase.java
@@ -1,39 +1,54 @@
 package edu.stanford.bmir.protege.web.shared.dispatch;
 
-import com.google.common.collect.ImmutableList;
-import edu.stanford.bmir.protege.web.shared.color.Color;
-import edu.stanford.bmir.protege.web.shared.issues.AddEntityCommentResult;
-import edu.stanford.bmir.protege.web.shared.issues.Comment;
-import edu.stanford.bmir.protege.web.shared.issues.ThreadId;
-import edu.stanford.bmir.protege.web.shared.match.JsonSerializationTestUtil;
-import edu.stanford.bmir.protege.web.shared.project.ProjectId;
-import edu.stanford.bmir.protege.web.shared.tag.AddProjectTagAction;
-import edu.stanford.bmir.protege.web.shared.tag.AddProjectTagResult;
-import edu.stanford.bmir.protege.web.shared.tag.Tag;
-import edu.stanford.bmir.protege.web.shared.tag.TagId;
+import edu.stanford.bmir.protege.web.shared.permissions.PermissionDeniedException;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import java.io.IOException;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Matthew Horridge
  * Stanford Center for Biomedical Informatics Research
- * 2021-04-05
+ * 28 Oct 2018
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ActionExecutionResult_TestCase {
 
+    @Mock
+    private DispatchServiceResultContainer resultContainer;
+
+    @Mock
+    private PermissionDeniedException permissionDeniedException;
+
+    @Mock
+    private ActionExecutionException actionExecutionException;
+
     @Test
-    public void shouldSerializeResult() throws IOException {
-        var nestedResult = AddProjectTagResult.create(Tag.get(
-                TagId.createTagId(),
-                ProjectId.getNil(),
-                "Tag",
-                "Desc",
-                Color.getWhite(),
-                Color.getWhite(),
-                ImmutableList.of()
-        ));
-        var result = ActionExecutionResult.get(DispatchServiceResultContainer.create(nestedResult));
-        JsonSerializationTestUtil.testSerialization(result, ActionExecutionResult.class);
+    public void shouldCreateResultFromResult() {
+        ActionExecutionResult result = ActionExecutionResult.get(resultContainer);
+        assertThat(result.getResult(), is(Optional.of(resultContainer)));
+        assertThat(result.getActionExecutionException().isPresent(), is(false));
+        assertThat(result.getPermissionDeniedException().isPresent(), is(false));
+    }
+
+    @Test
+    public void shouldCreateResultFromPermissionDeniedException() {
+        ActionExecutionResult result = ActionExecutionResult.get(permissionDeniedException);
+        assertThat(result.getPermissionDeniedException(), is(Optional.of(permissionDeniedException)));
+        assertThat(result.getResult().isPresent(), is(false));
+        assertThat(result.getActionExecutionException().isPresent(), is(false));
+    }
+
+    @Test
+    public void shouldCreateResultFromActionExecutionException() {
+        ActionExecutionResult result = ActionExecutionResult.get(actionExecutionException);
+        assertThat(result.getActionExecutionException(), is(Optional.of(actionExecutionException)));
+        assertThat(result.getResult().isPresent(), is(false));
+        assertThat(result.getPermissionDeniedException().isPresent(), is(false));
     }
 }
