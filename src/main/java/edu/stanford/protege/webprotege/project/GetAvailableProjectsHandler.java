0fc2cc6695cae16f18404dbdeaeca8f4d9c7652e
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-19 17:20:43 +0100

    Reimplemented handler for GetAvailableProjects

--- src/main/java/edu/stanford/protege/webprotege/project/GetAvailableProjectsHandler.java
+++ src/main/java/edu/stanford/protege/webprotege/project/GetAvailableProjectsHandler.java
@@ -1,25 +1,12 @@
 package edu.stanford.protege.webprotege.project;
 
-import edu.stanford.protege.webprotege.access.AccessManager;
-import edu.stanford.protege.webprotege.authorization.api.ProjectResource;
-import edu.stanford.protege.webprotege.authorization.api.Subject;
-import edu.stanford.protege.webprotege.common.ProjectId;
-import edu.stanford.protege.webprotege.common.UserId;
+import edu.stanford.protege.webprotege.api.ActionExecutor;
 import edu.stanford.protege.webprotege.ipc.CommandHandler;
 import edu.stanford.protege.webprotege.ipc.ExecutionContext;
 import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
-import edu.stanford.protege.webprotege.permissions.ProjectPermissionsManager;
-import edu.stanford.protege.webprotege.user.UserActivityManager;
-import edu.stanford.protege.webprotege.user.UserActivityRecord;
 import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Mono;
 
-import java.util.*;
-
-import static edu.stanford.protege.webprotege.access.BuiltInAction.DOWNLOAD_PROJECT;
-import static edu.stanford.protege.webprotege.access.BuiltInAction.MOVE_ANY_PROJECT_TO_TRASH;
-import static edu.stanford.protege.webprotege.authorization.api.Subject.forUser;
-import static java.util.stream.Collectors.toList;
 
 /**
  * Matthew Horridge
@@ -30,22 +17,16 @@ import static java.util.stream.Collectors.toList;
 public class GetAvailableProjectsHandler implements CommandHandler<GetAvailableProjectsAction, GetAvailableProjectsResult> {
 
 
-    private final GetAvailableProjectsActionHandler delegate;
+    private final ActionExecutor executor;
 
-    private UserActivityManager userActivityManager;
-
-    private ProjectPermissionsManager projectPermissionsManager;
-
-    private AccessManager accessManager;
-
-    public GetAvailableProjectsHandler(GetAvailableProjectsActionHandler delegate) {
-        this.delegate = Objects.requireNonNull(delegate);
+    public GetAvailableProjectsHandler(ActionExecutor executor) {
+        this.executor = executor;
     }
 
     @NotNull
     @Override
     public String getChannelName() {
-        return "projects.GetAvailableProjects";
+        return "project-management.GetAvailableProjects";
     }
 
     @Override
@@ -56,44 +37,6 @@ public class GetAvailableProjectsHandler implements CommandHandler<GetAvailableP
     @Override
     public Mono<GetAvailableProjectsResult> handleRequest(GetAvailableProjectsAction request,
                                                           ExecutionContext executionContext) {
-        UserId userId = executionContext.userId();
-        Optional<UserActivityRecord> userActivityRecord = userActivityManager.getUserActivityRecord(userId);
-        Map<ProjectId, Long> lastOpenedMap = new HashMap<>();
-        userActivityRecord.ifPresent(record -> record.getRecentProjects()
-                                                     .forEach(recent -> lastOpenedMap.put(recent.getProjectId(),
-                                                                                          recent.getTimestamp())));
-        List<AvailableProject> availableProjects = projectPermissionsManager.getReadableProjects(userId)
-                                                                            .stream()
-                                                                            .map(details -> {
-                                                                                Subject user = forUser(userId);
-                                                                                ProjectResource projectResource = new ProjectResource(
-                                                                                        details.getProjectId());
-                                                                                boolean downloadable = accessManager.hasPermission(
-                                                                                        user,
-                                                                                        projectResource,
-                                                                                        DOWNLOAD_PROJECT);
-                                                                                boolean trashable = details.getOwner()
-                                                                                                           .equals(userId) || accessManager.hasPermission(
-                                                                                        user,
-                                                                                        projectResource,
-                                                                                        MOVE_ANY_PROJECT_TO_TRASH);
-                                                                                long lastOpened = lastOpenedMap.getOrDefault(
-                                                                                        details.getProjectId(),
-                                                                                        0L);
-                                                                                return AvailableProject.get(details.getProjectId(),
-                                                                                                            details.getDisplayName(),
-                                                                                                            details.getDescription(),
-                                                                                                            details.getOwner(),
-                                                                                                            details.isInTrash(),
-                                                                                                            details.getCreatedAt(),
-                                                                                                            details.getCreatedBy(),
-                                                                                                            details.getLastModifiedAt(),
-                                                                                                            details.getLastModifiedBy(),
-                                                                                                            downloadable,
-                                                                                                            trashable,
-                                                                                                            lastOpened);
-                                                                            })
-                                                                            .collect(toList());
-        return Mono.just(GetAvailableProjectsResult.create(availableProjects));
+        return Mono.just(executor.execute(request, executionContext));
     }
 }
