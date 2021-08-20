29bb45a7735d9a69fd469b6071f5ab3dd90e820d
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:16:33 +0100

    Create GetObjectPropertyFrameCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/frame/GetObjectPropertyFrameCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.frame;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetObjectPropertyFrameCommandHandler implements CommandHandler<GetObjectPropertyFrameAction, GetObjectPropertyFrameResult> {
+
+    private final ActionExecutor executor;
+
+    public GetObjectPropertyFrameCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetObjectPropertyFrameAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetObjectPropertyFrameAction> getRequestClass() {
+        return GetObjectPropertyFrameAction.class;
+    }
+
+    @Override
+    public Mono<GetObjectPropertyFrameResult> handleRequest(GetObjectPropertyFrameAction request,
+                                                            ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
