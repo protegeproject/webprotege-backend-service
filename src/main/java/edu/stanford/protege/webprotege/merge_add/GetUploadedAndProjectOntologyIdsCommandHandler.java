a6950765329ae97fcf63e5dd92481a3c3b60d6bb
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:03:29 +0100

    Create GetUploadedAndProjectOntologyIdsCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/merge_add/GetUploadedAndProjectOntologyIdsCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.merge_add;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetUploadedAndProjectOntologyIdsCommandHandler implements CommandHandler<GetUploadedAndProjectOntologyIdsAction, GetUploadedAndProjectOntologyIdsResult> {
+
+    private final ActionExecutor executor;
+
+    public GetUploadedAndProjectOntologyIdsCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetUploadedAndProjectOntologyIdsAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetUploadedAndProjectOntologyIdsAction> getRequestClass() {
+        return GetUploadedAndProjectOntologyIdsAction.class;
+    }
+
+    @Override
+    public Mono<GetUploadedAndProjectOntologyIdsResult> handleRequest(GetUploadedAndProjectOntologyIdsAction request,
+                                                                      ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
