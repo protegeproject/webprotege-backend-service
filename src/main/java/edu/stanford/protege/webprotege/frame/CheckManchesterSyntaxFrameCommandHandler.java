bb55cfb315107d89a316e9711569f8e22b1afae3
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 14:46:11 +0100

    Added command handler for CheckManchesterSyntaxFrame

---
+++ src/main/java/edu/stanford/protege/webprotege/frame/CheckManchesterSyntaxFrameCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.frame;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class CheckManchesterSyntaxFrameCommandHandler implements CommandHandler<CheckManchesterSyntaxFrameAction, CheckManchesterSyntaxFrameResult> {
+
+    private final ActionExecutor executor;
+
+    public CheckManchesterSyntaxFrameCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return CheckManchesterSyntaxFrameAction.CHANNEL;
+    }
+
+    @Override
+    public Class<CheckManchesterSyntaxFrameAction> getRequestClass() {
+        return CheckManchesterSyntaxFrameAction.class;
+    }
+
+    @Override
+    public Mono<CheckManchesterSyntaxFrameResult> handleRequest(CheckManchesterSyntaxFrameAction request,
+                                                                ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
