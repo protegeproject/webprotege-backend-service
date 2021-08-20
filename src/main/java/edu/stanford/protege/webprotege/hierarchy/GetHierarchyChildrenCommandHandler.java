b1adce0daf6eb3917e6e5c0c1046af7d13194bad
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:12:47 +0100

    Create GetHierarchyChildrenCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/hierarchy/GetHierarchyChildrenCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.hierarchy;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetHierarchyChildrenCommandHandler implements CommandHandler<GetHierarchyChildrenAction, GetHierarchyChildrenResult> {
+
+    private final ActionExecutor executor;
+
+    public GetHierarchyChildrenCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetHierarchyChildrenAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetHierarchyChildrenAction> getRequestClass() {
+        return GetHierarchyChildrenAction.class;
+    }
+
+    @Override
+    public Mono<GetHierarchyChildrenResult> handleRequest(GetHierarchyChildrenAction request,
+                                                          ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
