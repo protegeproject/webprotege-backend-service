521052190dcaec60fb2f66a250370a03f4c32d29
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:20:30 +0100

    Create GetPerspectiveLayoutCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/perspective/GetPerspectiveLayoutCommandHandler.java
@@ -0,0 +1,39 @@
+package edu.stanford.protege.webprotege.perspective;
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetPerspectiveLayoutCommandHandler implements CommandHandler<GetPerspectiveLayoutAction, GetPerspectiveLayoutResult> {
+
+    private final ActionExecutor executor;
+
+    public GetPerspectiveLayoutCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetPerspectiveLayoutAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetPerspectiveLayoutAction> getRequestClass() {
+        return GetPerspectiveLayoutAction.class;
+    }
+
+    @Override
+    public Mono<GetPerspectiveLayoutResult> handleRequest(GetPerspectiveLayoutAction request,
+                                             ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
