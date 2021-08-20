92daa425148d94e22a24fc9266294d6dbc1ff8ad
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:12:12 +0100

    Update GetCurrentUserInSessionCommandHandler.java

--- src/main/java/edu/stanford/protege/webprotege/user/GetCurrentUserInSessionCommandHandler.java
+++ src/main/java/edu/stanford/protege/webprotege/user/GetCurrentUserInSessionCommandHandler.java
@@ -1,4 +1,4 @@
-package edu.stanford.protege.webprotege.entity;
+package edu.stanford.protege.webprotege.user;
 
 import edu.stanford.protege.webprotege.api.ActionExecutor;
 import edu.stanford.protege.webprotege.ipc.CommandHandler;
