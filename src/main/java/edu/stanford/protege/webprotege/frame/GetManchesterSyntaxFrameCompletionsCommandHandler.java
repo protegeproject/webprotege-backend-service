98466183d1d37e82f97a730a64e1439c9763a68e
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:15:34 +0100

    Create GetManchesterSyntaxFrameCompletionsCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/frame/GetManchesterSyntaxFrameCompletionsCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.frame;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetManchesterSyntaxFrameCompletionsCommandHandler implements CommandHandler<GetManchesterSyntaxFrameCompletionsAction, GetManchesterSyntaxFrameCompletionsResult> {
+
+    private final ActionExecutor executor;
+
+    public GetManchesterSyntaxFrameCompletionsCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetManchesterSyntaxFrameCompletionsAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetManchesterSyntaxFrameCompletionsAction> getRequestClass() {
+        return GetManchesterSyntaxFrameCompletionsAction.class;
+    }
+
+    @Override
+    public Mono<GetManchesterSyntaxFrameCompletionsResult> handleRequest(GetManchesterSyntaxFrameCompletionsAction request,
+                                                                         ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
