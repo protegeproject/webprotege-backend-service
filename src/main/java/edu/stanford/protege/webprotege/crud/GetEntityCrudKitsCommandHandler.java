cce16b79009005482f0879e3d0371531f06ca646
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:07:41 +0100

    Create GetEntityCrudKitsCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/crud/GetEntityCrudKitsCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.crud;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetEntityCrudKitsCommandHandler implements CommandHandler<GetEntityCrudKitsAction, GetEntityCrudKitsResult> {
+
+    private final ActionExecutor executor;
+
+    public GetEntityCrudKitsCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetEntityCrudKitsAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetEntityCrudKitsAction> getRequestClass() {
+        return GetEntityCrudKitsAction.class;
+    }
+
+    @Override
+    public Mono<GetEntityCrudKitsResult> handleRequest(GetEntityCrudKitsAction request,
+                                                       ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
