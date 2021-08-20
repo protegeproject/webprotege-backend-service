f5514be7747f6513270e15355b51fd2a6df1997b
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:08:46 +0100

    Create GetEntityDiscussionThreadsCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/issues/GetEntityDiscussionThreadsCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.issues;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetEntityDiscussionThreadsCommandHandler implements CommandHandler<GetEntityDiscussionThreadsAction, GetEntityDiscussionThreadsResult> {
+
+    private final ActionExecutor executor;
+
+    public GetEntityDiscussionThreadsCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetEntityDiscussionThreadsAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetEntityDiscussionThreadsAction> getRequestClass() {
+        return GetEntityDiscussionThreadsAction.class;
+    }
+
+    @Override
+    public Mono<GetEntityDiscussionThreadsResult> handleRequest(GetEntityDiscussionThreadsAction request,
+                                                                ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
