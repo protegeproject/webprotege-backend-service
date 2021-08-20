e5dd128aa6362b6947aacf27d13acf49d67fb486
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 14:57:50 +0100

    Create GetAnnotationPropertyFrameCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/frame/GetAnnotationPropertyFrameCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.frame;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetAnnotationPropertyFrameCommandHandler implements CommandHandler<GetAnnotationPropertyFrameAction, GetAnnotationPropertyFrameResult> {
+
+    private final ActionExecutor executor;
+
+    public GetAnnotationPropertyFrameCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetAnnotationPropertyFrameAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetAnnotationPropertyFrameAction> getRequestClass() {
+        return GetAnnotationPropertyFrameAction.class;
+    }
+
+    @Override
+    public Mono<GetAnnotationPropertyFrameResult> handleRequest(GetAnnotationPropertyFrameAction request,
+                                                                ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
