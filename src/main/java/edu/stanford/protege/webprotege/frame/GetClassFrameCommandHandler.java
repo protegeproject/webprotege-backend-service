5d7a8115767d74c16fa04568ad416f652b3ef699
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:08:49 +0100

    Update GetClassFrameCommandHandler.java

--- src/main/java/edu/stanford/protege/webprotege/frame/GetClassFrameCommandHandler.java
+++ src/main/java/edu/stanford/protege/webprotege/frame/GetClassFrameCommandHandler.java
@@ -1,4 +1,4 @@
-package edu.stanford.protege.webprotege.project;
+package edu.stanford.protege.webprotege.frame;
 
 import edu.stanford.protege.webprotege.api.ActionExecutor;
 import edu.stanford.protege.webprotege.ipc.CommandHandler;
