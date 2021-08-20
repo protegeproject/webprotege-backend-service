4662ae03c6466b462a00886c83599dd1d76f6abd
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:16:18 +0100

    Create GetNamedIndividualFrameCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/frame/GetNamedIndividualFrameCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.frame;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetNamedIndividualFrameCommandHandler implements CommandHandler<GetNamedIndividualFrameAction, GetNamedIndividualFrameResult> {
+
+    private final ActionExecutor executor;
+
+    public GetNamedIndividualFrameCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetNamedIndividualFrameAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetNamedIndividualFrameAction> getRequestClass() {
+        return GetNamedIndividualFrameAction.class;
+    }
+
+    @Override
+    public Mono<GetNamedIndividualFrameResult> handleRequest(GetNamedIndividualFrameAction request,
+                                                             ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
