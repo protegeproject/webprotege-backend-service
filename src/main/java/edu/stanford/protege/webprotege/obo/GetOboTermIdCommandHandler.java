5202de712c32de07cb2cf24418a66915fecfec33
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:17:18 +0100

    Create GetOboTermIdCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/obo/GetOboTermIdCommandHandler.java
@@ -0,0 +1,39 @@
+package edu.stanford.protege.webprotege.obo;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetOboTermIdCommandHandler implements CommandHandler<GetOboTermIdAction, GetOboTermIdResult> {
+
+    private final ActionExecutor executor;
+
+    public GetOboTermIdCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetOboTermIdAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetOboTermIdAction> getRequestClass() {
+        return GetOboTermIdAction.class;
+    }
+
+    @Override
+    public Mono<GetOboTermIdResult> handleRequest(GetOboTermIdAction request, ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
