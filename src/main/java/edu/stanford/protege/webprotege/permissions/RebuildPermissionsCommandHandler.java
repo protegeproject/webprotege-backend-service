5d3c97c40207a403278716173ea3f4ca11f2b8b1
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 14:58:35 +0100

    Create RebuildPermissionsCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/permissions/RebuildPermissionsCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.permissions;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class RebuildPermissionsCommandHandler implements CommandHandler<RebuildPermissionsAction, RebuildPermissionsResult> {
+
+    private final ActionExecutor executor;
+
+    public RebuildPermissionsCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return RebuildPermissionsAction.CHANNEL;
+    }
+
+    @Override
+    public Class<RebuildPermissionsAction> getRequestClass() {
+        return RebuildPermissionsAction.class;
+    }
+
+    @Override
+    public Mono<RebuildPermissionsResult> handleRequest(RebuildPermissionsAction request,
+                                                        ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
