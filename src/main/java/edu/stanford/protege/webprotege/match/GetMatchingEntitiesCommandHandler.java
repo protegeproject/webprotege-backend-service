574ec0cee27ab183bb0ebcd3573cbabc327aae19
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:15:53 +0100

    Create GetMatchingEntitiesCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/match/GetMatchingEntitiesCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.match;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetMatchingEntitiesCommandHandler implements CommandHandler<GetMatchingEntitiesAction, GetMatchingEntitiesResult> {
+
+    private final ActionExecutor executor;
+
+    public GetMatchingEntitiesCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetMatchingEntitiesAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetMatchingEntitiesAction> getRequestClass() {
+        return GetMatchingEntitiesAction.class;
+    }
+
+    @Override
+    public Mono<GetMatchingEntitiesResult> handleRequest(GetMatchingEntitiesAction request,
+                                                         ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
