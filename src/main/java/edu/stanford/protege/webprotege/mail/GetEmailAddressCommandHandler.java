162350c1f3f855a88e4577743f37049994c0b3b4
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:07:17 +0100

    Create GetEmailAddressCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/mail/GetEmailAddressCommandHandler.java
@@ -0,0 +1,39 @@
+package edu.stanford.protege.webprotege.mail;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetEmailAddressCommandHandler implements CommandHandler<GetEmailAddressAction, GetEmailAddressResult> {
+
+    private final ActionExecutor executor;
+
+    public GetEmailAddressCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetEmailAddressAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetEmailAddressAction> getRequestClass() {
+        return GetEmailAddressAction.class;
+    }
+
+    @Override
+    public Mono<GetEmailAddressResult> handleRequest(GetEmailAddressAction request, ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
