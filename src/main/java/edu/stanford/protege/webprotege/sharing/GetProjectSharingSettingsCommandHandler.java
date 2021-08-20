870846ece13d4c4b9970d2c16bced46bbfbdc41a
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:30:04 +0100

    Create GetProjectSharingSettingsCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/sharing/GetProjectSharingSettingsCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.sharing;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetProjectSharingSettingsCommandHandler implements CommandHandler<GetProjectSharingSettingsAction, GetProjectSharingSettingsResult> {
+
+    private final ActionExecutor executor;
+
+    public GetProjectSharingSettingsCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetProjectSharingSettingsAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetProjectSharingSettingsAction> getRequestClass() {
+        return GetProjectSharingSettingsAction.class;
+    }
+
+    @Override
+    public Mono<GetProjectSharingSettingsResult> handleRequest(GetProjectSharingSettingsAction request,
+                                                               ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
