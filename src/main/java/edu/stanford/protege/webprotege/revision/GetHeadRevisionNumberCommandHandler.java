6ffe3f714bdd5ab777ae6871fa77bf8e40e661c3
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-20 15:11:20 +0100

    Create GetHeadRevisionNumberCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/revision/GetHeadRevisionNumberCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.revision;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-20
+ */
+@WebProtegeHandler
+public class GetHeadRevisionNumberCommandHandler implements CommandHandler<GetHeadRevisionNumberAction, GetHeadRevisionNumberResult> {
+
+    private final ActionExecutor executor;
+
+    public GetHeadRevisionNumberCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return GetHeadRevisionNumberAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetHeadRevisionNumberAction> getRequestClass() {
+        return GetHeadRevisionNumberAction.class;
+    }
+
+    @Override
+    public Mono<GetHeadRevisionNumberResult> handleRequest(GetHeadRevisionNumberAction request,
+                                                           ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
