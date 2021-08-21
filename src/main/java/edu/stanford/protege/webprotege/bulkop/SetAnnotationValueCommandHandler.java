75e2566e0c8dda5491cc040e3c91ccfb72ac7d08
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-21 12:01:28 +0100

    Create SetAnnotationValueCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/bulkop/SetAnnotationValueCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.bulkop;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-21
+ */
+@WebProtegeHandler
+public class SetAnnotationValueCommandHandler implements CommandHandler<SetAnnotationValueAction, SetAnnotationValueResult> {
+
+    private final ActionExecutor executor;
+
+    public SetAnnotationValueCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return SetAnnotationValueAction.CHANNEL;
+    }
+
+    @Override
+    public Class<SetAnnotationValueAction> getRequestClass() {
+        return SetAnnotationValueAction.class;
+    }
+
+    @Override
+    public Mono<SetAnnotationValueResult> handleRequest(SetAnnotationValueAction request,
+                                                        ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
