90709b18ce535ecbd9ae85eeb1e0bb65c375f3b7
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-21 12:04:38 +0100

    Create SetEntityGraphActiveFiltersCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/viz/SetEntityGraphActiveFiltersCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.viz;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-21
+ */
+@WebProtegeHandler
+public class SetEntityGraphActiveFiltersCommandHandler implements CommandHandler<SetEntityGraphActiveFiltersAction, SetEntityGraphActiveFiltersResult> {
+
+    private final ActionExecutor executor;
+
+    public SetEntityGraphActiveFiltersCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return SetEntityGraphActiveFiltersAction.CHANNEL;
+    }
+
+    @Override
+    public Class<SetEntityGraphActiveFiltersAction> getRequestClass() {
+        return SetEntityGraphActiveFiltersAction.class;
+    }
+
+    @Override
+    public Mono<SetEntityGraphActiveFiltersResult> handleRequest(SetEntityGraphActiveFiltersAction request,
+                                                                 ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
