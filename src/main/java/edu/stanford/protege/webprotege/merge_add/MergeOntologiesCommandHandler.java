6dcabdfc06cc1604c323cdc99824d51e483833bb
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-21 11:56:48 +0100

    Create MergeOntologiesCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/merge_add/MergeOntologiesCommandHandler.java
@@ -0,0 +1,39 @@
+package edu.stanford.protege.webprotege.merge_add;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-21
+ */
+@WebProtegeHandler
+public class MergeOntologiesCommandHandler implements CommandHandler<MergeOntologiesAction, MergeOntologiesResult> {
+
+    private final ActionExecutor executor;
+
+    public MergeOntologiesCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return MergeOntologiesAction.CHANNEL;
+    }
+
+    @Override
+    public Class<MergeOntologiesAction> getRequestClass() {
+        return MergeOntologiesAction.class;
+    }
+
+    @Override
+    public Mono<MergeOntologiesResult> handleRequest(MergeOntologiesAction request, ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
