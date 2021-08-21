96012f8b63264c2f76306f709c3660f147c8e385
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-21 12:12:19 +0100

    Create SetProjectTagsCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/tag/SetProjectTagsCommandHandler.java
@@ -0,0 +1,39 @@
+package edu.stanford.protege.webprotege.tag;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-21
+ */
+@WebProtegeHandler
+public class SetProjectTagsCommandHandler implements CommandHandler<SetProjectTagsAction, SetProjectTagsResult> {
+
+    private final ActionExecutor executor;
+
+    public SetProjectTagsCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return SetProjectTagsAction.CHANNEL;
+    }
+
+    @Override
+    public Class<SetProjectTagsAction> getRequestClass() {
+        return SetProjectTagsAction.class;
+    }
+
+    @Override
+    public Mono<SetProjectTagsResult> handleRequest(SetProjectTagsAction request, ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
