1f8f9b84b3709a30e467695c460379fe322ad079
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-21 11:59:24 +0100

    Create ResetPerspectivesCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/perspective/ResetPerspectivesCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.perspective;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-21
+ */
+@WebProtegeHandler
+public class ResetPerspectivesCommandHandler implements CommandHandler<ResetPerspectivesAction, ResetPerspectivesResult> {
+
+    private final ActionExecutor executor;
+
+    public ResetPerspectivesCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return ResetPerspectivesAction.CHANNEL;
+    }
+
+    @Override
+    public Class<ResetPerspectivesAction> getRequestClass() {
+        return ResetPerspectivesAction.class;
+    }
+
+    @Override
+    public Mono<ResetPerspectivesResult> handleRequest(ResetPerspectivesAction request,
+                                                       ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
