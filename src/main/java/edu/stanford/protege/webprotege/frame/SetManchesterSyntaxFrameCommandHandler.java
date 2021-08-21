fb1714c3767da4bbbdaefecca07097d4616a4b23
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-21 12:05:26 +0100

    Create SetManchesterSyntaxFrameCommandHandler.java

---
+++ src/main/java/edu/stanford/protege/webprotege/frame/SetManchesterSyntaxFrameCommandHandler.java
@@ -0,0 +1,40 @@
+package edu.stanford.protege.webprotege.frame;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.CommandHandler;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.ipc.WebProtegeHandler;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-08-21
+ */
+@WebProtegeHandler
+public class SetManchesterSyntaxFrameCommandHandler implements CommandHandler<SetManchesterSyntaxFrameAction, SetManchesterSyntaxFrameResult> {
+
+    private final ActionExecutor executor;
+
+    public SetManchesterSyntaxFrameCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @NotNull
+    @Override
+    public String getChannelName() {
+        return SetManchesterSyntaxFrameAction.CHANNEL;
+    }
+
+    @Override
+    public Class<SetManchesterSyntaxFrameAction> getRequestClass() {
+        return SetManchesterSyntaxFrameAction.class;
+    }
+
+    @Override
+    public Mono<SetManchesterSyntaxFrameResult> handleRequest(SetManchesterSyntaxFrameAction request,
+                                                              ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
