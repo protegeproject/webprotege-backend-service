beb15901017e240594602f4a57359ae648168a9a
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-21 16:13:18 +0100

    Migrated to junit 5

--- src/test/java/edu/stanford/protege/webprotege/issues/AddComment_TestCase.java
+++ src/test/java/edu/stanford/protege/webprotege/issues/AddComment_TestCase.java
@@ -8,7 +8,7 @@ import edu.stanford.protege.webprotege.event.EventTag;
 import edu.stanford.protege.webprotege.match.JsonSerializationTestUtil;
 import edu.stanford.protege.webprotege.common.ProjectId;
 import edu.stanford.protege.webprotege.common.UserId;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.Optional;
