6e6e20e2c9227536b0008ac78a053cba0a507dd6
Matthew Horridge <matthew.horridge@stanford.edu>
2021-10-27 16:16:28 -0700

    Renamed projectOwner to owner

--- src/main/java/edu/stanford/protege/webprotege/project/NewProjectSettings.java
+++ src/main/java/edu/stanford/protege/webprotege/project/NewProjectSettings.java
@@ -22,7 +22,7 @@ import java.util.Optional;
 public abstract class NewProjectSettings {
 
 
-    private static final String PROJECT_OWNER = "projectOwner";
+    private static final String PROJECT_OWNER = "owner";
 
     private static final String DISPLAY_NAME = "displayName";
 
