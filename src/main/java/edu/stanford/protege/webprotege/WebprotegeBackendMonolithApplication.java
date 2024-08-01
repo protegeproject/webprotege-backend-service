4233c5c9b679a8a6fea4b684225ece417181cace
Matthew Horridge <matthew.horridge@stanford.edu>
2024-08-01 16:23:50 -0700

    Added OWLDataFactory bean that was previously in an imported config

--- src/main/java/edu/stanford/protege/webprotege/WebprotegeBackendMonolithApplication.java
+++ src/main/java/edu/stanford/protege/webprotege/WebprotegeBackendMonolithApplication.java
@@ -1,10 +1,14 @@
 package edu.stanford.protege.webprotege;
 
 import edu.stanford.protege.webprotege.ipc.WebProtegeIpcApplication;
+import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
 
 @SpringBootApplication
 public class WebprotegeBackendMonolithApplication {
@@ -13,4 +17,9 @@ public class WebprotegeBackendMonolithApplication {
 		SpringApplication.run(WebprotegeBackendMonolithApplication.class, args);
 	}
 
+	@Bean
+	@ConditionalOnMissingBean
+	OWLDataFactory dataFactory() {
+		return new OWLDataFactoryImpl();
+	}
 }
