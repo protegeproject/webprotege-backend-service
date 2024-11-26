a2d4aef04970518125b6d59e1efa93238571b3d0
soimugeo <soimugeo@gmail.com>
2024-11-26 13:31:54 +0200

    Fix for CreateClassesFromApiAction

--- src/main/java/edu/stanford/protege/webprotege/icd/actions/CreateClassesFromApiAction.java
+++ src/main/java/edu/stanford/protege/webprotege/icd/actions/CreateClassesFromApiAction.java
@@ -1,7 +1,6 @@
 package edu.stanford.protege.webprotege.icd.actions;
 
 import com.fasterxml.jackson.annotation.*;
-import com.google.common.collect.ImmutableSet;
 import edu.stanford.protege.webprotege.common.*;
 import edu.stanford.protege.webprotege.dispatch.ProjectAction;
 
@@ -14,7 +13,7 @@ public record CreateClassesFromApiAction(@JsonProperty("changeRequestId") Change
                                          @JsonProperty("projectId") ProjectId projectId,
                                          @JsonProperty("sourceText") String sourceText,
                                          @JsonProperty("langTag") @Nullable String langTag,
-                                         @JsonProperty("parent") ImmutableSet<String> parent) implements ProjectAction<CreateClassesFromApiResult> {
+                                         @JsonProperty("parent") String parent) implements ProjectAction<CreateClassesFromApiResult> {
     public static final String CHANNEL = "icatx.webprotege.entities.CreateClassesFromApi";
 
     public String getChannel() {
