21cd5ae893eec4dc71a8b8b96eb2b61a21796c89
soimugeo <soimugeo@gmail.com>
2025-03-17 20:42:10 +0200

    [https://github.com/who-icatx/icatx-project/issues/167]: Added GetEntityDirectParentsActionHandler

---
+++ src/main/java/edu/stanford/protege/webprotege/entity/GetEntityDirectParentsActionHandler.java
@@ -0,0 +1,65 @@
+package edu.stanford.protege.webprotege.entity;
+
+import edu.stanford.protege.webprotege.access.*;
+import edu.stanford.protege.webprotege.dispatch.AbstractProjectActionHandler;
+import edu.stanford.protege.webprotege.hierarchy.ClassHierarchyProvider;
+import edu.stanford.protege.webprotege.ipc.ExecutionContext;
+import edu.stanford.protege.webprotege.renderer.RenderingManager;
+import jakarta.inject.Inject;
+
+import javax.annotation.*;
+
+import static edu.stanford.protege.webprotege.access.BuiltInAction.VIEW_PROJECT;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 16 Jun 2017
+ */
+public class GetEntityDirectParentsActionHandler extends AbstractProjectActionHandler<GetEntityDirectParentsAction, GetEntityDirectParentsResult> {
+
+    @Nonnull
+    private final ClassHierarchyProvider classHierarchyProvider;
+    @Nonnull
+    private final EntityNodeRenderer entityNodeRenderer;
+
+    @Nonnull
+    private final RenderingManager renderingManager;
+
+    @Inject
+    public GetEntityDirectParentsActionHandler(@Nonnull AccessManager accessManager,
+                                               @Nonnull ClassHierarchyProvider classHierarchyProvider,
+                                               @Nonnull EntityNodeRenderer entityNodeRenderer,
+                                               @Nonnull RenderingManager renderingManager) {
+        super(accessManager);
+        this.classHierarchyProvider = classHierarchyProvider;
+        this.entityNodeRenderer = entityNodeRenderer;
+        this.renderingManager = renderingManager;
+    }
+
+    @Nonnull
+    @Override
+    public Class<GetEntityDirectParentsAction> getActionClass() {
+        return GetEntityDirectParentsAction.class;
+    }
+
+    @Nullable
+    @Override
+    protected BuiltInAction getRequiredExecutableBuiltInAction(GetEntityDirectParentsAction action) {
+        return VIEW_PROJECT;
+    }
+
+    @Nonnull
+    @Override
+    public GetEntityDirectParentsResult execute(@Nonnull GetEntityDirectParentsAction action,
+                                                @Nonnull ExecutionContext executionContext) {
+        var parents = classHierarchyProvider.getParents(action.entity().asOWLClass())
+                .stream()
+                .map(entityNodeRenderer::render)
+                .toList();
+        var entityData = renderingManager.getRendering(action.entity());
+        return new GetEntityDirectParentsResult(entityData, parents);
+    }
+
+
+}
---
+++ src/main/java/edu/stanford/protege/webprotege/entity/GetEntityDirectParentsCommandHandler.java
@@ -0,0 +1,39 @@
+package edu.stanford.protege.webprotege.entity;
+
+import edu.stanford.protege.webprotege.api.ActionExecutor;
+import edu.stanford.protege.webprotege.ipc.*;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2021-09-01
+ */
+@WebProtegeHandler
+public class GetEntityDirectParentsCommandHandler implements CommandHandler<GetEntityDirectParentsAction, GetEntityDirectParentsResult> {
+
+    private final ActionExecutor executor;
+
+    public GetEntityDirectParentsCommandHandler(ActionExecutor executor) {
+        this.executor = executor;
+    }
+
+    @Nonnull
+    @Override
+    public String getChannelName() {
+        return GetEntityDirectParentsAction.CHANNEL;
+    }
+
+    @Override
+    public Class<GetEntityDirectParentsAction> getRequestClass() {
+        return GetEntityDirectParentsAction.class;
+    }
+
+    @Override
+    public Mono<GetEntityDirectParentsResult> handleRequest(GetEntityDirectParentsAction request,
+                                                           ExecutionContext executionContext) {
+        return Mono.just(executor.execute(request, executionContext));
+    }
+}
