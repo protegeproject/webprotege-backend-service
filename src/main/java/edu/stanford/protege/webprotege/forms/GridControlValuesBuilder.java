386e628a3dc4470fededcaf626b63a842e53b8d4
Matthew Horridge <matthew.horridge@stanford.edu>
2025-07-14 14:30:18 -0700

    Use custom page sizes instead of default page size

--- src/main/java/edu/stanford/protege/webprotege/forms/GridControlValuesBuilder.java
+++ src/main/java/edu/stanford/protege/webprotege/forms/GridControlValuesBuilder.java
@@ -120,7 +120,8 @@ public class GridControlValuesBuilder {
 
         var pageRequest = rootSubject.map(s -> formPageRequestIndex.getPageRequest(s,
                                                                                formFieldId,
-                                                                               FormPageRequest.SourceType.GRID_CONTROL)).orElseGet(() -> PageRequest.requestPageWithSize(1, descriptor.getPageSize()));
+                                                                               FormPageRequest.SourceType.GRID_CONTROL,
+                descriptor.getPageSize())).orElseGet(() -> PageRequest.requestPageWithSize(1, descriptor.getPageSize()));
         var comparator = comparatorFactory.get(descriptor, Optional.empty());
 
         var subjectFactoryDescriptor = descriptor.getSubjectFactoryDescriptor();
